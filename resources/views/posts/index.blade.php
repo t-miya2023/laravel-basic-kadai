4b234152cd251fd2c77cdf4c8c2eabe073664b87
t-miya2023 <t.miyata2023@gmail.com>
2023-11-25 06:53:35 +0900

    kadai2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+
+<body>
+    <h1>投稿一覧</h1>
+</body>
